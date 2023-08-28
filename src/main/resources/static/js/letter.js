4b07da17077a4b9806e24dda8554b691b9f0b08a
lijianjian <lijian3.141@icloud.com>
2023-08-28 15:14:42 +0800

    私信列表、发送私信

--- src/main/resources/static/js/letter.js
+++ src/main/resources/static/js/letter.js
@@ -5,10 +5,31 @@ $(function(){
 
 function send_letter() {
 	$("#sendModal").modal("hide");
-	$("#hintModal").modal("show");
-	setTimeout(function(){
-		$("#hintModal").modal("hide");
-	}, 2000);
+
+    // 获取标题和内容
+    var toName = $("#recipient-name").val();
+    var content = $("#message-text").val();
+	// 发送异步请求
+    $.post(
+        CONTEXT_PATH + "/letter/send",
+        {"toName":toName,"content":content},
+        function(data) {
+            data = $.parseJSON(data);
+//    	        if (data.code == 0) {
+                $("#hintBody").text(data.msg);
+//    	        } else {
+//    	            $("#hintBody").text("发送成功！");
+//    	        }
+
+            // 显示提示框
+            $("#hintModal").modal("show");
+            // 2秒后，自动隐藏提示框
+            setTimeout(function(){
+                $("#hintModal").modal("hide");
+                    window.location.reload();
+            }, 2000);
+        }
+    );
 }
 
 function delete_msg() {

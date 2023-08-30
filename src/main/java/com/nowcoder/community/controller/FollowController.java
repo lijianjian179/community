d1bb1e5faa3eac373f649a2feb16fcb693e18292
lijianjian <lijian3.141@icloud.com>
2023-08-30 14:25:33 +0800

    80%自主完成关注列表、粉丝列表

--- src/main/java/com/nowcoder/community/controller/FollowController.java
+++ src/main/java/com/nowcoder/community/controller/FollowController.java
@@ -1,23 +1,34 @@
 package com.nowcoder.community.controller;
 
+import com.nowcoder.community.entity.Page;
 import com.nowcoder.community.entity.User;
 import com.nowcoder.community.service.FollowService;
+import com.nowcoder.community.service.UserService;
+import com.nowcoder.community.util.CommunityConstant;
 import com.nowcoder.community.util.CommunityUtil;
 import com.nowcoder.community.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 @Controller
-public class FollowController {
+public class FollowController implements CommunityConstant {
 
     @Autowired
     private FollowService followService;
 
     @Autowired
     private HostHolder hostHolder;
+
+    @Autowired
+    private UserService userService;
 
     @RequestMapping(path = "/follow", method = RequestMethod.POST)
     @ResponseBody
@@ -37,5 +48,71 @@ public class FollowController {
         followService.unFollow(user.getId(), entityType, entityId);
 
         return CommunityUtil.getJSONString(0, "已取消关注");
+    }
+
+    @RequestMapping(path = "/followee/{userId}", method = RequestMethod.GET)
+    public String getFolloweePage(@PathVariable("userId") int userId, Model model, Page page) {
+        User user = userService.findUserById(userId);
+        if (user == null) {
+            throw new RuntimeException("该用户不存在！");
+        }
+
+        // 设置分页信息
+        page.setLimit(5);
+        page.setPath("/followee/" + userId);
+        page.setRows((int) followService.findFolloweeCount(userId, ENTITY_TYPE_USER));
+
+        List<Map<Integer, Double>> followList = followService.findFolloweeList(userId, ENTITY_TYPE_USER, page.getOffset(), page.getLimit());
+        List<Map<String, Object>> followVoList = new ArrayList<>();
+        if (followList != null) {
+            for (Map<Integer, Double> map : followList) {
+                Map<String, Object> voMap = new HashMap<>();
+                for (Integer key : map.keySet()) {
+                    voMap.put("user", userService.findUserById(key));
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    String followTime = dateFormat.format(map.get(key));
+                    voMap.put("followTime", followTime);
+                }
+                followVoList.add(voMap);
+            }
+        }
+
+        model.addAttribute("user", user);
+        model.addAttribute("followees", followVoList);
+
+        return "/site/followee";
+    }
+
+    @RequestMapping(path = "/follower/{userId}", method = RequestMethod.GET)
+    public String getFollowerPage(@PathVariable("userId") int userId, Model model, Page page) {
+        User user = userService.findUserById(userId);
+        if (user == null) {
+            throw new RuntimeException("该用户不存在！");
+        }
+
+        // 设置分页信息
+        page.setLimit(5);
+        page.setPath("/follower/" + userId);
+        page.setRows((int) followService.findFollowerCount(userId, ENTITY_TYPE_USER));
+
+        List<Map<Integer, Double>> followerList = followService.findFollowerList(userId, ENTITY_TYPE_USER, page.getOffset(), page.getLimit());
+        List<Map<String, Object>> followerVoList = new ArrayList<>();
+        if (followerList != null) {
+            for (Map<Integer, Double> map : followerList) {
+                Map<String, Object> voMap = new HashMap<>();
+                for (Integer key : map.keySet()) {
+                    voMap.put("user", userService.findUserById(key));
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    String followTime = dateFormat.format(map.get(key));
+                    voMap.put("followedTime", followTime);
+                }
+                followerVoList.add(voMap);
+            }
+        }
+
+        model.addAttribute("user", user);
+        model.addAttribute("followers", followerVoList);
+
+        return "/site/follower";
     }
 }
--- src/main/java/com/nowcoder/community/service/FollowService.java
+++ src/main/java/com/nowcoder/community/service/FollowService.java
@@ -8,6 +8,8 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.SessionCallback;
 import org.springframework.stereotype.Service;
 
+import java.util.*;
+
 @Service
 public class FollowService {
 
@@ -62,5 +64,34 @@ public class FollowService {
     public boolean hasFollowed(int userId, int entityType, int entityId) {
         String followeeKey = RedisKeyUtil.getFolloweeKey(userId, entityType);
         return redisTemplate.opsForZSet().score(followeeKey, entityId) != null;
+    }
+
+    // 查询当前用户关注的实体列表
+    public List<Map<Integer, Double>> findFolloweeList(int userId, int entityType, int offset, int limit) {
+        String followeeKey = RedisKeyUtil.getFolloweeKey(userId, entityType);
+        List<Map<Integer, Double>> followeeList = new ArrayList<>();
+        // 倒转排序，实现关注时间往后的显示在前
+        Set<Integer> set = redisTemplate.opsForZSet().reverseRange(followeeKey, offset, offset + limit -1);
+        for (Integer entityId : set) {
+            Map<Integer, Double> map = new HashMap<>();
+            map.put(entityId, redisTemplate.opsForZSet().score(followeeKey, entityId));
+            followeeList.add(map);
+        }
+        return followeeList;
+    }
+
+    // 查询实体的粉丝列表
+    public List<Map<Integer, Double>> findFollowerList(int entityId, int entityType, int offset, int limit) {
+        String followerKey = RedisKeyUtil.getFollowerKey(entityId, entityType);
+        List<Map<Integer, Double>> followerList = new ArrayList<>();
+        // 倒转排序，实现关注时间往后的显示在前
+        Set<Integer> set = redisTemplate.opsForZSet().reverseRange(followerKey, offset, offset + limit -1);
+        for (Integer id : set) {
+            Map<Integer, Double> map = new HashMap<>();
+            map.put(id, redisTemplate.opsForZSet().score(followerKey, id));
+            followerList.add(map);
+        }
+
+        return followerList;
     }
 }

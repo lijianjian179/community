84c3c90f0a93b4e975a789f5f6763fd7055f4239
lijianjian <lijian3.141@icloud.com>
2023-09-04 19:37:37 +0800

    将文件上传至云服务器。文件类型：图片。服务器：七牛云服务器。

--- src/main/java/com/nowcoder/community/controller/ShareController.java
+++ src/main/java/com/nowcoder/community/controller/ShareController.java
@@ -40,13 +40,16 @@ public class ShareController implements CommunityConstant {
     @Value("${wk.image.storage}")
     private String wkImageStorage;
 
+    @Value("${qiniu.bucket.share.url}")
+    private String shareBucketUrl;
+
     @RequestMapping(path = "/share", method = RequestMethod.GET)
     @ResponseBody
     public String share(String htmlUrl) {
         // 文件名
         String fileName = CommunityUtil.generateUUID();
 
-        // 异步地生成长图
+        // 异步生成长图
         Event event = new Event()
                 .setTopic(TOPIC_SHARE)
                 .setData("htmlUrl", htmlUrl)
@@ -56,11 +59,13 @@ public class ShareController implements CommunityConstant {
 
         // 返回访问路径
         Map<String, Object> map = new HashMap<>();
-        map.put("shareUrl", domain + contextPath + "/share/image/" + fileName);
+//        map.put("shareUrl", domain + contextPath + "/share/image/" + fileName);
+        map.put("shareUrl", shareBucketUrl + "/" + fileName);
 
         return CommunityUtil.getJSONString(0, null, map);
     }
 
+    // 废弃
     // 获取长图
     @RequestMapping(path = "/share/image/{fileName}", method = RequestMethod.GET)
     public void getShareImage(@PathVariable("fileName") String fileName, HttpServletResponse response) {
